1e3a24b1569ab829f5418f1be32f14608a036659
edRibeiro <eder.sonz@hotmail.com>
2017-01-26 16:39:22 -0200

    Inicio do trabalho 4

---
+++ Trabalho 4/Trabalho_4_DeAnaliseDescisoria.py
@@ -0,0 +1,9 @@
+def main():
+    print ("    Topicos Avançados")
+    print ("    Analise Descisoria\n")
+    print ("    Trabalho: Método de Análise Hierárquica\n") 
+    print ("Foco Principal: Escolha de um altomóvel")
+    print ("Conjunto de alternativas viáveis:")
+    print ("Conjunto de critérios:")
+
+main()
